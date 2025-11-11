949fb905cf8f03882cdda376dcee1f26da423f58
cypherab01 <aghimire074@gmail.com>
2025-11-11 21:50:30 +0545

    misc

--- constants/experience.ts
+++ constants/experience.ts
@@ -3,20 +3,13 @@ import { TimelineViewerData } from '@/types/TimelineViewer.types';
 export const experiences: TimelineViewerData[] = [
   {
     title: 'Mobile and Web Developer · Mantra IT',
-    date: 'Mar. 2025 - Present',
-    description: `Currently building responsive web and mobile applications, including
-    an air ticketing platform and E-Grow farming app, using React,
-    Next.js, and React Native at Mantra IT.`,
+    date: 'Jan. 2025 - Present',
+    description: `At Mantra I.T. Solution Pvt. Ltd., I develop and maintain web and mobile applications using Next.js, React, and React Native. I work on the Harvestmoon Flight Booking Website, creating responsive UIs with Tailwind CSS and shadcn/ui, implementing secure authentication with Auth.js, and integrating REST APIs with Axios. I also contribute to the eGrow - Easy Farming app, fixing bugs, adding features, and maintaining reusable components, strengthening my skills in clean code, scalable architecture, and user-focused solutions.`,
     latest: true,
   },
   {
-    title: 'Next.js Developer Internship · Treeleaf Technologies',
-    date: 'Feb. 2025 - Mar. 2025',
-    description: `Interned as a Next.js Developer at Treeleaf Technologies, developing and maintaining front-end components for web applications using React and Next.js while mastering modern web development practices.`,
-  },
-  {
-    title: 'CloudWorker (Part-time) · CloudFactory',
-    date: 'Dec. 2023 - Present',
+    title: 'CloudWorker · CloudFactory',
+    date: 'Dec. 2023 - Present (Part-time)',
     description: `Working as a part-time CloudWorker at CloudFactory, where I annotate diverse datasets to support AI and machine learning projects while honing my attention to detail and time management skills.`,
   },
 ];
